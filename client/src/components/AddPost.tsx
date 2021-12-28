6ca7d4ad31e570a125bccd53736d9a474425d187
Arjun Anand <arjunanand1405@gmail.com>
2021-12-28 16:19:21 -0600

    minor refactoring

--- client/src/components/AddPost.tsx
+++ client/src/components/AddPost.tsx
@@ -6,20 +6,23 @@ interface Props {}
 const AddPost: React.FC<Props> = (props: Props) => {
 	const MAX_HEIGHT_OF_TEXTAREA = 600;
 	const [addPostVal, setAddPostVal] = useState("");
+
+	const handleTypePost = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setAddPostVal(e.target.value);
+		e.target.style.height = "0";
+		e.target.style.height =
+			e.target.scrollHeight < MAX_HEIGHT_OF_TEXTAREA
+				? `calc(${e.target.scrollHeight}px + .5em)`
+				: `${MAX_HEIGHT_OF_TEXTAREA}px`;
+	};
+
 	return (
 		<div className="AddPost row">
 			<img src="https://picsum.photos/200" alt="pfp" />
 			<textarea
 				value={addPostVal}
 				placeholder="Say something..."
-				onChange={e => {
-					setAddPostVal(e.target.value);
-					e.target.style.height = "0";
-					e.target.style.height =
-						e.target.scrollHeight < MAX_HEIGHT_OF_TEXTAREA
-							? `calc(${e.target.scrollHeight}px + .5em)`
-							: `${MAX_HEIGHT_OF_TEXTAREA}px`;
-				}}
+				onChange={handleTypePost}
 			/>
 			<button onClick={() => setAddPostVal("")}>Send</button>
 		</div>
